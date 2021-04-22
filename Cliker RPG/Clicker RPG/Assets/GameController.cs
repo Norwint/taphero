56d5d4a86f64e4ab4b27790cc5d9e084047d7437
Norwint <71719363+Norwint@users.noreply.github.com>
2021-04-22 18:21:28 +0200

    enemytext/stagetext/coins

--- Cliker RPG/Clicker RPG/Assets/GameController.cs
+++ Cliker RPG/Clicker RPG/Assets/GameController.cs
@@ -9,8 +9,12 @@ public class GameController : MonoBehaviour
     public double health;
     public double healthCap;
     public double dpc;
+    public double enemy;
+    public double stage;
 
     public Text healthText;
+    public Text enemyText;
+    public Text stageText;
 
     public Image healthbar;
 
@@ -19,11 +23,16 @@ public class GameController : MonoBehaviour
         healthCap = 20;
         health = healthCap;
         dpc = 1;
+        enemy = 0;
+        stage = 1;
     }
 
     public void Update()
     {
         healthText.text = health + "/" + healthCap + " HP";
+        enemyText.text = enemy + "/" + "10";
+        stageText.text = "Stage - " + stage;
+
         healthbar.fillAmount = (float)(health / healthCap);
     }
 
@@ -35,6 +44,14 @@ public class GameController : MonoBehaviour
         if (health <= 0)
         {
             health = healthCap;
+
+            enemy += 1;
+
+            if (enemy > 10)
+            {
+                enemy = 0;
+                stage = stage + 1;
+            }
 
         }
     }
