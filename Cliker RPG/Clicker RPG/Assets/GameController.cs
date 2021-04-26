dccdd88e9693daca6836ef5a247f258e28467e02
Norwint <71719363+Norwint@users.noreply.github.com>
2021-04-26 16:41:03 +0200

    update money

--- Cliker RPG/Clicker RPG/Assets/GameController.cs
+++ Cliker RPG/Clicker RPG/Assets/GameController.cs
@@ -11,11 +11,13 @@ public class GameController : MonoBehaviour
     public double dpc;
     public double enemy;
     public double stage;
+    public double money;
 
     public Text healthText;
     public Text enemyText;
     public Text stageText;
     public Text dpsText;
+    public Text moneyText;
 
     public Image healthbar;
 
@@ -26,6 +28,7 @@ public class GameController : MonoBehaviour
         dpc = 1;
         enemy = 0;
         stage = 1;
+        money = 0;
     }
 
     public void Update()
@@ -33,6 +36,7 @@ public class GameController : MonoBehaviour
         healthText.text = health + "/" + healthCap + " HP";
         enemyText.text = enemy + "/" + "10";
         stageText.text = "Stage - " + stage;
+        moneyText.text = money.ToString();
         dpsText.text = "" + dpc;
 
         healthbar.fillAmount = (float)(health / healthCap);
@@ -40,7 +44,6 @@ public class GameController : MonoBehaviour
 
     public void Hit()
     {
-        Debug.Log("HE ENTRADO");
         health = health - dpc;
 
         if (health <= 0)
@@ -49,12 +52,23 @@ public class GameController : MonoBehaviour
 
             enemy += 1;
 
+            money += Random.Range(1, 5);
+
             if (enemy > 10)
             {
                 enemy = 0;
                 stage = stage + 1;
             }
 
+        }
+    }
+
+    public void BuyDPS()
+    {
+        if (money >= 10)
+        {
+            dpc = 5;
+            money -= 10;
         }
     }
 }
